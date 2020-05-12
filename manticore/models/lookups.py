534ed58dc1cb030db80b58ae1aa08909a0910a87
tumb1er <zimbler@gmail.com>
2020-05-12 15:31:24 +0300

    docstrings and comments

--- manticore/models/lookups.py
+++ manticore/models/lookups.py
@@ -4,20 +4,29 @@ from django.utils.datastructures import OrderedSet
 
 
 class MultiExact(lookups.Exact):
+    """ Implements equality comparison for multi fields with IN-function."""
 
     def as_sql(self, compiler, connection):
         return InFunction(self.lhs, [self.rhs]).as_sql(compiler, connection)
 
 
 class InFunction(lookups.In):
+    """ Implements function IN(needle, haystack...)."""
 
     def as_sql(self, compiler, connection):
+        """
+        Formats IN function call
+        """
         lhs_sql, params = self.process_lhs(compiler, connection)
         rhs_sql, rhs_params = self.process_rhs(compiler, connection)
         params.extend(rhs_params)
         return 'IN(%s, %s)' % (lhs_sql, rhs_sql), params
 
     def process_rhs(self, compiler, connection):
+        """
+        Forms placeholder list without parentheses like "%s %s %s", for use
+        with function call IN(%s, %s, %s, %s).
+        """
         if self.rhs_is_direct_value():
             try:
                 rhs = OrderedSet(self.rhs)
